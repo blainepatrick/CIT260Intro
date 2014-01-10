97f29fd6089be4c9dc69f0dbeb3a6cbf94f2de4e
blainepatrick <blainepatrick@netscape.net>
2014-01-10 09:51:04 -0600

    Updates left braces - New line!!!

--- src/gallons2liters/Gallons2Liters.java
+++ src/gallons2liters/Gallons2Liters.java
@@ -8,20 +8,23 @@ package gallons2liters;
  *
  * @author PATRICKS
  */
-public class Gallons2Liters {
+public class Gallons2Liters
+{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        double gallons; //kuntaynz tha numba of gallunz
-        double liters; //kuntaynz tha numba of leetuhz
+    public static void main(String[] args)
+    {
         
-        gallons = 10; //initializes tha numba of gallunz
-        liters = gallons * 3.8754; //initializes tha numba of gallunz
+        double galz; //kuntaynz tha numba of gallunz
+        double leetz; //kuntaynz tha numba of leetuhz
+        
+        galz = 10; //initializes tha numba of gallunz
+        leetz = galz * 3.8754; //initializes tha numba of gallunz
         
         //outputs the conversion to the screen
-        System.out.println(gallons + " gallons is " + liters + " liters" + "\n" + "Peace Out!!!");
+        System.out.println(galz + " gallons is " + leetz + " liters" + "\n" + "Peace Out!!!");
         
     }
 }
