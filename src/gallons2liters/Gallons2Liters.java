5ff4b6c56d34cf89484cf5085e6ab8b21eea03af
blainepatrick <blainepatrick@netscape.net>
2014-01-13 15:30:29 -0600

    Update

--- src/gallons2liters/Gallons2Liters.java
+++ src/gallons2liters/Gallons2Liters.java
@@ -17,8 +17,8 @@ public class Gallons2Liters
     public static void main(String[] args)
     {
         
-        double galz; //kuntaynz tha numba of gallunz
-        double leetz; //kuntaynz tha numba of leetuhz
+        double galz; //kuntaynz tha numba of gallunz.
+        double leetz; //kuntaynz tha numba of leetuhz.
         
         galz = 10; //initializes tha numba of gallunz
         leetz = galz * 3.8754; //initializes tha numba of gallunz
